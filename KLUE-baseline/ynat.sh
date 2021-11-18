ea8c3fab8b3462ea5d9b934b4b6749c5fa9be511
taeheejeon <taeheejeon22@gmail.com>
2021-11-18 22:35:33 +0900

    dp, ynat

--- KLUE-baseline/ynat.sh
+++ KLUE-baseline/ynat.sh
@@ -13,25 +13,26 @@ read gpus
 echo "gpus == ${gpus}"
 
 
-tasks="ynat"
+task="ynat"
 
 tokenizers=("eojeol_mecab_fixed_composed_grammatical_symbol_F_wp-64k" 
 "eojeol_mecab_fixed_decomposed_pure_grammatical_symbol_F_wp-64k"
 "morpheme_mecab_orig_composed__grammatical_symbol_F_wp-64k"
 "morpheme_mecab_orig_decomposed_pure_grammatical_symbol_F_wp-64k"
-"morpheme_mecab_fixed_composed_grammatical_symbol_F_wp-64k"
-"morpheme_mecab_fixed_decomposed_pure_grammatical_symbol_F_wp-64k"
-"morpheme_mecab_fixed_composed_grammatical_symbol_T_wp-64k"
-"morpheme_mecab_fixed_decomposed_pure_grammatical_symbol_T_wp-64k"
-"morpheme_mecab_fixed_decomposed_lexical_grammatical_symbol_T_wp-64k"
-"morpheme_mecab_fixed_decomposed_grammatical_grammatical_symbol_T_wp-64k")
+"morpheme_mecab_fixed_composed_grammatical_symbol_F_wp-64k")
+
+# "morpheme_mecab_fixed_decomposed_pure_grammatical_symbol_F_wp-64k"
+# "morpheme_mecab_fixed_composed_grammatical_symbol_T_wp-64k"
+# "morpheme_mecab_fixed_decomposed_pure_grammatical_symbol_T_wp-64k"
+# "morpheme_mecab_fixed_decomposed_lexical_grammatical_symbol_T_wp-64k"
+# "morpheme_mecab_fixed_decomposed_grammatical_grammatical_symbol_T_wp-64k"
 
 # 각 resource(tokenizer) 마다 task 수행
 
 for tokenizer in "${tokenizers[@]}"; do
     # resource directory
-    resource="../resources/v6_without_dummy_letter_grammatical_symbol_F/${tokenizer}"
-
+    resources="../resources/v6_without_dummy_letter_grammatical_symbol_F/${tokenizer}"
+    echo "###### tokenizer: ${tokenizer} #########"
     python run_klue.py train \
     --task ${task} \
     --output_dir ${OUTPUT_DIR}  \
