784a3f8a940f938779294ef041744d57593b20de
Taehee Jeon <taeheejeon22@gmail.com>
2021-11-19 20:25:08 +0900

    update_dp

--- KLUE-baseline/klue_baseline/data/klue_dp.py
+++ KLUE-baseline/klue_baseline/data/klue_dp.py
@@ -235,15 +235,17 @@ class KlueDPProcessor(DataProcessor):
                 else:
                     token_list = [token.replace("\n", "") for token in line.split("\t")] + ["-", "-"]
 
-                    ### our
+                    ### our ### text를 형태소 분석 후 토큰들 갈아끼워 넣기
+                    # 1 유희열이 -> 유희열 이
+                    # 4 탈락에 -> 탈락 에
+
                     new_token_list = token_list[:]
-                    # new_token_list[1] = " ".join( new_tokens[int(token_list[0])-1] )
                     new_token_list[1] = new_tokens[int(token_list[0]) - 1]
 
-                    print(f"my_token_0: {new_tokens[int(token_list[0])-1]}")
-                    print(f"my_token_1: {new_token_list[1]}")
-                    print(f"\norig_token: {token_list[1]}")
-                    print(f"new_token: {new_token_list[1]}\n")
+                    # print(f"my_token_0: {new_tokens[int(token_list[0])-1]}")
+                    # print(f"my_token_1: {new_token_list[1]}")
+                    # print(f"\norig_token: {token_list[1]}")
+                    # print(f"new_token: {new_token_list[1]}\n")
 
                     # print(f"token_list:{token_list}\n")  ### our ###
                     # print(f"new_token_list:{new_token_list}\n")  ### our ###
@@ -258,7 +260,7 @@ class KlueDPProcessor(DataProcessor):
                             sent_id=sent_id,
                             token_id=int(token_list[0]),
                             # token=token_list[1],
-                            token=new_token_list[1],
+                            token=new_token_list[1],    ### our ###
                             pos=token_list[3],
                             head=token_list[4],
                             dep=token_list[5],
@@ -522,19 +524,17 @@ class KlueDPProcessor(DataProcessor):
             batch_bpe_tail_masks.append(bpe_tail_mask)
 
 
-            ### our # from dependency_parsing.py
-            # head_ids = [i for i, token in enumerate(bpe_head_mask[batch_id]) if token == 1]
-            head_ids = [i for i, token in enumerate(batch[batch_id][2]) if token == 1]
-            # tail_ids = [i for i, token in enumerate(bpe_tail_mask[batch_id]) if token == 1]
-            tail_ids = [i for i, token in enumerate(batch[batch_id][3]) if token == 1]
-
-            print(f"orig_text: {self.tokenizer.decode(batch[batch_id][0])}")
-
-            print(f"<klue_dp> batch[batch_id]:{batch[batch_id]}")
-            print(f"\nlen(head_ids):{len(head_ids)}\n len(tail_ids): {len(tail_ids)}")  ### our ###
-
-            assert len(head_ids) == len(tail_ids)
-            ###
+            # ### our # from dependency_parsing.py
+            # head_ids = [i for i, token in enumerate(batch[batch_id][2]) if token == 1]
+            # tail_ids = [i for i, token in enumerate(batch[batch_id][3]) if token == 1]
+            #
+            # print(f"orig_text: {self.tokenizer.decode(batch[batch_id][0])}")
+            #
+            # print(f"<klue_dp> batch[batch_id]:{batch[batch_id]}")
+            # print(f"\nlen(head_ids):{len(head_ids)}\n len(tail_ids): {len(tail_ids)}")  ### our ###
+            #
+            # assert len(head_ids) == len(tail_ids)
+            # ###
 
 
         # 2. build inputs : packing tensors
