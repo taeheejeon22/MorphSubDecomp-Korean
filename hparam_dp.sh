8b840e09ff40561543f077a59812b01af19e2c19
kyle-bong <wdg369@gmail.com>
2021-11-24 15:16:32 +0900

    오타 수정

--- hparam_dp.sh
+++ hparam_dp.sh
@@ -17,7 +17,7 @@ tokenizers=("morpheme_mecab_orig_composed_grammatical_symbol_F_wp-64k"
 "morpheme_mecab_fixed_decomposed_pure_grammatical_symbol_F_wp-64k"
 "morpheme_mecab_fixed_composed_grammatical_symbol_T_wp-64k"
 "morpheme_mecab_fixed_decomposed_lexical_grammatical_symbol_T_wp-64k"
-"morpheme_mecab_fixed_decomposed_grammatical_grammatical_symbol_T_wp-64k"
+"morpheme_mecab_fixed_decomposed_grammatical_pure_symbol_T_wp-64k"
 )
 
     #"eojeol_mecab_fixed_composed_grammatical_symbol_F_wp-64k"
