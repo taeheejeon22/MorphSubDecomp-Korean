c9021452f5db24ee8543961ef80fe4f2663b454a
taeheejeon <taeheejeon22@gmail.com>
2021-11-24 16:45:42 +0900

    hparam sh

--- hparam_dp.sh
+++ hparam_dp.sh
@@ -11,13 +11,9 @@ echo -e "gpu num 0 1 2 3 ? "
 read gpu_num
 echo "gpu_num == ${gpu_num}"
 
-tokenizers=("morpheme_mecab_orig_composed_grammatical_symbol_F_wp-64k"
-"morpheme_mecab_orig_decomposed_pure_grammatical_symbol_F_wp-64k"
-"morpheme_mecab_fixed_composed_grammatical_symbol_F_wp-64k"
-"morpheme_mecab_fixed_decomposed_pure_grammatical_symbol_F_wp-64k"
-"morpheme_mecab_fixed_composed_grammatical_symbol_T_wp-64k"
-"morpheme_mecab_fixed_decomposed_lexical_grammatical_symbol_T_wp-64k"
-"morpheme_mecab_fixed_decomposed_grammatical_pure_symbol_T_wp-64k"
+tokenizers=("morpheme_mecab_orig_composed_grammatical_symbol_F_wp-64k" "morpheme_mecab_orig_decomposed_pure_grammatical_symbol_F_wp-64k" "morpheme_mecab_fixed_composed_grammatical_symbol_F_wp-64k"
+"morpheme_mecab_fixed_decomposed_pure_grammatical_symbol_F_wp-64k" "morpheme_mecab_fixed_composed_grammatical_symbol_T_wp-64k"
+"morpheme_mecab_fixed_decomposed_lexical_grammatical_symbol_T_wp-64k" "morpheme_mecab_fixed_decomposed_grammatical_pure_symbol_T_wp-64k"
 )
 
     #"eojeol_mecab_fixed_composed_grammatical_symbol_F_wp-64k"
@@ -47,7 +43,7 @@ for batch_size in "${batch_sizes[@]}"; do
               echo "### log_dir: $log_dir ###"
               echo "### summary_dir: $summary_dir ###"
 
-              for tokenizer in "${tokenizers}"; do
+              for tokenizer in "${tokenizers[@]}"; do
                   echo "### tokenizer: ${tokenizer} ###"
 
                   # resource dir
--- hparam_korsts.sh
+++ hparam_korsts.sh
@@ -12,16 +12,12 @@ echo -e "gpu num 0 1 2 3 ? "
 read gpu_num
 echo "gpu_num == ${gpu_num}"
 
-tokenizers=("morpheme_mecab_orig_composed_grammatical_symbol_F_wp-64k"
-"morpheme_mecab_orig_decomposed_pure_grammatical_symbol_F_wp-64k"
-"morpheme_mecab_fixed_composed_grammatical_symbol_F_wp-64k"
-"morpheme_mecab_fixed_decomposed_pure_grammatical_symbol_F_wp-64k"
-"morpheme_mecab_fixed_composed_grammatical_symbol_T_wp-64k"
-"morpheme_mecab_fixed_decomposed_lexical_grammatical_symbol_T_wp-64k"
-"morpheme_mecab_fixed_decomposed_grammatical_pure_symbol_T_wp-64k"
+tokenizers=("morpheme_mecab_orig_decomposed_pure_grammatical_symbol_F_wp-64k" "morpheme_mecab_fixed_composed_grammatical_symbol_F_wp-64k"
+"morpheme_mecab_fixed_decomposed_pure_grammatical_symbol_F_wp-64k" "morpheme_mecab_fixed_composed_grammatical_symbol_T_wp-64k"
+"morpheme_mecab_fixed_decomposed_lexical_grammatical_symbol_T_wp-64k" "morpheme_mecab_fixed_decomposed_grammatical_pure_symbol_T_wp-64k"
 )
 
-
+# "morpheme_mecab_orig_composed_grammatical_symbol_F_wp-64k"
 # 각 배치사이즈, 각 학습률 별로 태스크를 수행함.
 # 에포크 수는 5회로 통일.
 
@@ -40,7 +36,7 @@ for batch_size in "${batch_sizes[@]}"; do
             echo "### log_dir: $log_dir ###"
             echo "### summary_dir: $summary_dir ###"
         
-            for tokenizer in "${tokenizers}"; do 
+            for tokenizer in "${tokenizers[@]}"; do
                 echo "### tokenizer: ${tokenizer} ###"
 
                 # resource dir
