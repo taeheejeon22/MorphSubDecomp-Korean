fa2fdfd2a815e5a067ea120cd426d504d3fc430d
bongseok <wdg369@gmail.com>
2023-10-30 20:33:00 +0900

    Delete Useless comments

--- KLUE-baseline/klue_baseline/data/base.py
+++ KLUE-baseline/klue_baseline/data/base.py
@@ -19,31 +19,6 @@ import inspect
 import os
 import sys
 
-# currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-# parentdir = os.path.dirname( os.path.dirname(currentdir) )
-# parentdir = os.path.dirname(currentdir)
-# sys.path.insert(0, parentdir)
-
-# from tokenizer import (
-#     # CharTokenizer,
-#     # JamoTokenizer,
-#     # MeCabSentencePieceTokenizer_orig,
-#     # MeCabSentencePieceTokenizer_fixed,
-#     # MeCabSentencePieceTokenizer,
-#     MeCabWordPieceTokenizer,
-#     # MeCabTokenizer,
-#     # MeCabTokenizer_orig,
-#     # MeCabTokenizer_fixed,    # MeCabSentencePieceTokenizer_kortok,
-#     MeCabTokenizer_all,
-#     # MeCabTokenizer_kortok,
-#     # SentencePieceTokenizer,
-#     WordPieceTokenizer,
-#     Vocab,
-#     # WordTokenizer,
-# )
-###
-
-
 logger = logging.getLogger(__name__)
 
 
@@ -114,15 +89,6 @@ class DataProcessor:
 
         ### our ###
         self.path_rsc = path_rsc
-
-        # with open(os.path.join(self.path_rsc, "tok.json")) as f:
-        #     tokenizer_config: dict = json.load(f)
-        #
-        # # wp = WordPieceTokenizer(os.path.join(self.path_rsc, "bert_tokenizer.json"))
-        #
-        # mecab = MeCabTokenizer_all(token_type=tokenizer_config["token_type"], tokenizer_type=tokenizer_config["tokenizer_type"], decomposition_type=tokenizer_config["decomposition_type"], space_symbol=tokenizer_config["space_symbol"], dummy_letter=tokenizer_config["dummy_letter"], nfd=tokenizer_config["nfd"], grammatical_symbol=tokenizer_config["grammatical_symbol"])
-        # self.tokenizer = MeCabWordPieceTokenizer(mecab=mecab, wp=tokenizer)  # mecab_wp.py
-        ###
 
 
     def get_train_dataset(self, data_dir: str, file_name: str) -> Dataset:
